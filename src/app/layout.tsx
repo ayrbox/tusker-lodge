8600e5c77cd161123b4ca009114f388f5ccb1230
ayrbox <sabin.dangol@hotmail.com>
2025-07-08 22:54:19 +0100

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Inter, Tinos } from "next/font/google";
+import { Tinos } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-const inter = Inter({
-  variable: "--font-inter-sans",
-  subsets: ["latin"],
-  weight: ["400"],
-});
 
 const tinos = Tinos({
   variable: "--font-tions-serif",
