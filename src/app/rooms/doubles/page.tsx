eadfd997f57b5b77679b692b185935c45d95d383
ayrbox <sabin.dangol@hotmail.com>
2025-07-24 22:36:48 +0100

    feat: update all rooms pages with correct features

--- src/app/rooms/doubles/page.tsx
+++ src/app/rooms/doubles/page.tsx
@@ -1,26 +1,84 @@
 import Image from "next/image";
 import DoublesRoomImage from "@/images/rooms/doubles.jpg";
-import ButtonLink from "@/components/ButtonLink";
+import Facilities from "@/app/rooms/Facilities";
+import Wrapper, { Sidebar, Content } from "@/app/rooms/Wrapper";
 
 export default function DoublesRoom() {
   return (
-    <>
-      <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-bold text-center text-gray-600">
-          Doubles Room
-        </h1>
-        <p className="mt-4 pb-8 w-full md:w-1/2 text-gray-400">
-          <strong>Room 1</strong> Double room with 1 double bed.
-        </p>
+    <div className="flex flex-col items-center">
+      <Image
+        src={DoublesRoomImage}
+        alt="Doubles Room - Room 1"
+        className="pb-4"
+      />
 
-        <Image
-          src={DoublesRoomImage}
-          alt="Doubles Room - Room 1"
-          className="pb-4"
-        />
+      <Wrapper title="Single Rooms">
+        <Sidebar>
+          <Facilities />
+        </Sidebar>
+        <Content>
+          <div>
+            <h3 className="sr-only">Description</h3>
+            <div className="space-y-6">
+              <p className="text-base text-gray-900">
+                Our Double Rooms includes double bed and well furnished for your
+                comfort stay. We have three equally sized double bed rooms.
+              </p>
 
-        <ButtonLink href="./booking">Book Now</ButtonLink>
-      </div>
-    </>
+              <p className="text-base text-gray-900">
+                <strong>Room 03</strong> is north facing and is situated on the
+                first floor. It is a “compact” double room with a standard 4’6”
+                double bed and en-suite shower room.
+              </p>
+
+              <p className="text-base text-gray-900">
+                <strong>Room 10</strong> is south facing and is situated on the
+                second floor. It is a “compact” king room with a 5&apos; king
+                &ldquo;zip-link&rdquo; bed and en-suite shower room.
+              </p>
+
+              <p className="text-base text-gray-900">
+                <strong>Room 11</strong> is south facing and is situated on the
+                second floor. It is a “compact” double room with a standard 4’6”
+                double bed and en-suite shower room.
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10">
+            <h2 className="text-sm font-medium text-gray-900">Amenites</h2>
+            <div className="mt-4">
+              <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                <li className="text-gray-400">
+                  <span className="text-gray-600">24&ldquo; TV</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Free Wifi</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Pet Friendly</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Tea and Coffee</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Hair Dryer</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="mt-10">
+            <h2 className="text-sm font-medium text-gray-900">Breakfast</h2>
+            <div className="mt-4 space-y-6">
+              <p className="text-sm text-gray-600">
+                Start your day right with our delicious breakfast, an optional
+                extra at just £10 per person per day.
+              </p>
+            </div>
+          </div>
+        </Content>
+      </Wrapper>
+    </div>
   );
 }
--- src/app/rooms/queen/page.tsx
+++ src/app/rooms/queen/page.tsx
@@ -1,26 +1,65 @@
 import Image from "next/image";
 import QueenRoomImage from "@/images/rooms/queen.jpg";
-import ButtonLink from "@/components/ButtonLink";
+import Facilities from "@/app/rooms/Facilities";
+import Wrapper, { Sidebar, Content } from "@/app/rooms/Wrapper";
 
 export default function QueenRoom() {
   return (
-    <>
-      <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-bold text-center text-gray-600">
-          Queen Room
-        </h1>
-        <p className="mt-4 pb-8 w-full md:w-1/2 text-gray-400">
-          <strong>Room 3</strong> Queen room with 1 queen bed.
-        </p>
+    <div className="flex flex-col items-center">
+      <Image src={QueenRoomImage} alt="Queen Room - Room 3" className="pb-4" />
+      <Wrapper title="Room 3: Queen Room ">
+        <Sidebar>
+          <Facilities />
+        </Sidebar>
+        <Content>
+          <div>
+            <h3 className="sr-only">Description</h3>
+            <div className="space-y-6">
+              <p className="text-base text-gray-900">
+                Queen Room is our super king sized double, a spacious north
+                facing, first floor en-suite with a 6′ super king, sumptuously
+                comfortable double bed and a compact en-suite.
+              </p>
+            </div>
+          </div>
 
-        <Image
-          src={QueenRoomImage}
-          alt="Queen Room - Room 3"
-          className="pb-4"
-        />
+          <div className="mt-10">
+            <h2 className="text-sm font-medium text-gray-900">Amenites</h2>
+            <div className="mt-4">
+              <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Sleeps up to 3</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">50&ldquo; TV</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Free Wifi</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Pet Friendly</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Tea and Coffee</span>
+                </li>
+                <li className="text-gray-400">
+                  <span className="text-gray-600">Hair Dryer</span>
+                </li>
+              </ul>
+            </div>
+          </div>
 
-        <ButtonLink href="./booking">Book Now</ButtonLink>
-      </div>
-    </>
+          <div className="mt-10">
+            <h2 className="text-sm font-medium text-gray-900">Breakfast</h2>
+            <div className="mt-4 space-y-6">
+              <p className="text-sm text-gray-600">
+                Start your day right with our delicious breakfast, an optional
+                extra at just £10 per person per day.
+              </p>
+            </div>
+          </div>
+        </Content>
+      </Wrapper>
+    </div>
   );
 }
