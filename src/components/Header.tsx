4654a05d2cebd34fb7b956ecc22833fe55341562
ayrbox <sabin.dangol@hotmail.com>
2025-09-07 01:19:53 +0100

    sidebar menu for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,35 @@
+"use client";
+import { useState } from "react";
 import NavItem from "./NavItem";
 import Image from "next/image";
 
 import LogoImage from "@/images/logo.jpg";
 import ButtonLink from "./ButtonLink";
-import { PhoneCallIcon } from "lucide-react";
+import { PhoneCallIcon, MenuIcon, XIcon } from "lucide-react";
+import Link from "next/link";
+import clsx from "clsx";
 
 export default function Header() {
+  const [menu, setMenu] = useState(true);
+
+  const handleMenuToggle: React.MouseEventHandler = (e) => {
+    e.preventDefault();
+    setMenu((m) => !m);
+  };
+
   return (
-    <>
-      <div className="p-2 md:p-8">
-        <div className="container mx-auto py-3 flex justify-between items-center">
+    <div className="p-2">
+      <div className="container mx-auto py-3 flex justify-between items-center">
+        <Link href="/">
           <Image
             src={LogoImage}
             alt="Tusker Lodge Logo"
             width={200}
             height={60}
           />
+        </Link>
 
-          <ol className="hidden md:flex align-items gap-8">
-            <li>
-              <NavItem text="Home" url="/" />
-            </li>
-            <li>
-              <NavItem text="Our Rooms" url="/rooms" />
-            </li>
-            <li>
-              <NavItem text="Local Attraction" url="/attractions" />
-            </li>
-            <li>
-              <NavItem text="Pets" url="/pets" />
-            </li>
-            <li>
-              <NavItem text="Parking" url="/parking" />
-            </li>
-            <li>
-              <NavItem text="Contact" url="/contact" />
-            </li>
-          </ol>
-
-          <div className="hidden relative md:grid grid-cols-[auto_1fr] gap-x-2 items-center text-gray-700">
-            <div className="p-3 row-span-2 rounded-full bg-orange-500">
-              <PhoneCallIcon className="text-orange-100" />
-            </div>
-            <p className="text-gray-400">Call Us</p>
-            <p className="text-gray-700">
-              <a href="tel:+441803292668">
-                <span className="absolute inset-0"></span>
-                +44 1803 292668
-              </a>
-            </p>
-          </div>
-
-          <ButtonLink href="./book-now">Check Availability</ButtonLink>
-        </div>
-      </div>
-      <div className="p-2 md:p-8 flex flex-col md:space-y-4 justify-between items-center">
-        <div className="container mx-auto flex items-center justify-between md:pb-4 md:mb-4 md:border-b md:border-gray-300">
-          <p className="text-2xl hidden md:block text-gray-400">01803 292668</p>
-          <div className="">
-            <Image
-              src={LogoImage}
-              alt="Tusker Lodge Logo"
-              width={200}
-              height={60}
-            />
-          </div>
-          <ButtonLink href="./book-now">Book Now</ButtonLink>
-        </div>
-        <ol className="hidden md:flex align-items gap-8">
+        <ol className="hidden lg:flex align-items gap-4">
           <li>
             <NavItem text="Home" url="/" />
           </li>
@@ -87,7 +49,51 @@ export default function Header() {
             <NavItem text="Contact" url="/contact" />
           </li>
         </ol>
+
+        <div className="hidden relative md:grid grid-cols-[auto_1fr] gap-x-2 items-center text-gray-700">
+          <div className="p-3 row-span-2 rounded-full bg-orange-500">
+            <PhoneCallIcon className="text-orange-100" />
+          </div>
+          <p className="text-gray-400">Call Us</p>
+          <p className="text-gray-700">
+            <a href="tel:+441803292668">
+              <span className="absolute inset-0"></span>
+              +44 1803 292668
+            </a>
+          </p>
+        </div>
+
+        <ButtonLink href="https://booking-directly.com/widgets/DLdRvxNR4vFcZTkOiuz4Qo5FPd8WPmcwPjOPtcPJsAV1CDfo7uweeGDXgLdtm">
+          Check Availability
+        </ButtonLink>
+
+        <button onClick={handleMenuToggle}>
+          <MenuIcon />
+        </button>
+        <div
+          className={clsx(
+            "z-1 absolute inset-0 bg-gray-900 opacity-25",
+            menu && "block",
+            !menu && "hidden",
+          )}
+          role="button"
+          onClick={handleMenuToggle}
+        ></div>
+        <section
+          className={clsx(
+            "absolute top-0 bottom-0 right-0 bg-gray-200 z-1 transition-all duration-150 ease-in-out",
+            menu && "w-100",
+            !menu && "w-0",
+          )}
+        >
+          <div className="">
+            <h1>Menu Here</h1>
+            <button onClick={handleMenuToggle}>
+              <XIcon />
+            </button>
+          </div>
+        </section>
       </div>
-    </>
+    </div>
   );
 }
