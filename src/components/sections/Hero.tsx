1da8731344b308065592959439a0633132a8a222
ayrbox <sabin.dangol@hotmail.com>
2025-09-18 20:58:57 +0100

    fix: hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,32 @@
-import ButtonLink from "@/components/ButtonLink";
 import TorquayHarbourImage from "@/images/featured-torquay-harbour.jpg";
+import clsx from "clsx";
+import Link from "next/link";
+
+const HeroCTA = ({
+  href = "",
+  variant = "default",
+  children,
+}: {
+  href?: string;
+  variant?: "default" | "booking";
+  children: React.ReactNode;
+}) => (
+  <Link
+    href={href}
+    target="_blank"
+    className={clsx(
+      "border rounded-full border border-transparent px-8 py-3 text-base font-medium text-white focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden shadow",
+      {
+        "bg-[#ffa400] hover:bg-[#009ffd]": variant === "default",
+      },
+      {
+        "bg-[#003b95] hover:bg-[#006ce4]": variant === "booking",
+      },
+    )}
+  >
+    {children}
+  </Link>
+);
 
 export default function Hero() {
   //console.dir(HeroImage, { depth: Infinity });
@@ -19,10 +46,16 @@ export default function Hero() {
           Sandy welcomes you to Tusker Lodge in Torquay Harbour, located in
           heart of English Riviera.
         </p>
-        <div>
-          <ButtonLink href="https://booking-directly.com/widgets/DLdRvxNR4vFcZTkOiuz4Qo5FPd8WPmcwPjOPtcPJsAV1CDfo7uweeGDXgLdtm">
-            Book Now
-          </ButtonLink>
+        <div className="flex gap-4 justify-items-end">
+          <HeroCTA href={process.env.NEXT_PUBLIC_BOOKING_DIRECTLY}>
+            Book Directly
+          </HeroCTA>
+          <HeroCTA
+            href={process.env.NEXT_PUBLIC_BOOKING_DOT_COM}
+            variant="booking"
+          >
+            Booking.com
+          </HeroCTA>
         </div>
       </div>
     </section>
