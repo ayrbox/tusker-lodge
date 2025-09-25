48a4ab97e2c2fe12f0d92aca8d0285cdd7f4b5da
ayrbox <sabin.dangol@hotmail.com>
2025-09-25 20:38:47 +0100

    feat: update rooms cta

--- src/components/sections/Rooms.tsx
+++ src/components/sections/Rooms.tsx
@@ -4,6 +4,8 @@ import KingRoomImage from "@/images/rooms/king.jpg";
 import FamilyRoomImage from "@/images/rooms/family.jpg";
 import DoubleRoomImage from "@/images/rooms/singles.jpg";
 import SingleRoomImage from "@/images/rooms/singles.jpg";
+import Link from "next/link";
+import { ArrowRightIcon } from "lucide-react";
 
 const Room = (props: {
   image: StaticImageData;
@@ -24,8 +26,23 @@ const Room = (props: {
 
 export default function Rooms() {
   return (
-    <section className="flex flex-col lg:flex-row bg-gray-200">
-      <div className="lg:w-1/2 grid md:grid-cols-2">
+    <section className="flex flex-col xl:flex-row-reverse">
+      <div className="xl:w-1/2 px-8 py-8 flex justify-center items-center">
+        <div className="p-4 flex flex-col gap-8 text-center items-center">
+          <h1 className="text-3xl text-gray-700 font-bold">Our Rooms</h1>
+          <p className="text-gray-500">
+            Choose from 9 comfortable rooms – singles, doubles, or triples to
+            suit your needs. Each comes with TV and all essential amenities at
+            reasonable prices. Simple, comfortable, and everything you need for
+            a relaxing stay.
+          </p>
+          <Link className="flex" href="./rooms">
+            <span>Find more </span>
+            <ArrowRightIcon />
+          </Link>
+        </div>
+      </div>
+      <div className="xl:w-1/2 grid md:grid-cols-2">
         <Room
           name="Family Room"
           info="Starting from £50"
@@ -38,19 +55,6 @@ export default function Rooms() {
         />
         <Room name="Doubles" info="Starting from £50" image={DoubleRoomImage} />
         <Room name="Singles" info="Starting from £50" image={SingleRoomImage} />
-      </div>
-      <div className="lg:w-1/2 px-8 py-8 flex justify-center items-center">
-        <div className="p-4 flex flex-col gap-3 text-center justify-between">
-          <h1 className="text-3xl text-gray-700 font-bold">Our Rooms</h1>
-          <p className="text-gray-500">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal.
-          </p>
-          <div>
-            <ButtonLink href="./rooms">View Rooms</ButtonLink>
-          </div>
-        </div>
       </div>
     </section>
   );
