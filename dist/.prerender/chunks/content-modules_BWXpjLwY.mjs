777360ff265b0e8881ff66de06fb91ef5a73c4b0
ayrbox <sabin.dangol@hotmail.com>
2026-06-06 22:21:46 +0100

    fix: remove dist

--- dist/.prerender/chunks/content-modules_BWXpjLwY.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/content/rooms/king.mdx", () => import('./king_CBvAg-sk.mjs')]]);
-
-export { contentModules as default };
--- dist/.prerender/chunks/king_CBvAg-sk.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./king_Bs_j-4RJ.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
