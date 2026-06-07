ceb1d02406ed4c1a3bd64420e18a1f8763da07e3
ayrbox <sabin.dangol@hotmail.com>
2026-06-07 22:22:38 +0100

    feat: add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from "@astrojs/mdx";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
