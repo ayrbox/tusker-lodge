5e6aeeede415a988f2c2bb3e6f89f3555283b562
ayrbox <sabin.dangol@hotmail.com>
2026-09-20 23:04:08 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), icon()],
+  site: "https://tuskerlodge.co.uk",
+  integrations: [react(), mdx(), icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
